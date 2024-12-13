b058b9c9a05e1aa78b918d65fa19178f3ac4f095
Mauro Simoes <maurosimoes9@gmail.com>
2024-12-13 13:35:04 +0100

    init project files

---
+++ src/test/java/com/fullstack/projet/ProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
