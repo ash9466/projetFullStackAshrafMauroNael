fe6abe115e78dc0011f978507c601c3dc10bb826
Ashraf <ashrafsabaa1@gmail.com>
2025-01-10 15:37:17 +0100

    adding some exceptions on fields control

--- src/main/java/com/fullstack/projet/models/Feedback.java
+++ src/main/java/com/fullstack/projet/models/Feedback.java
@@ -1,5 +1,6 @@
 package com.fullstack.projet.models;
 
+import com.fullstack.projet.exceptions.ValidationException;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,13 +8,17 @@ import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
+import jakarta.persistence.*;
+
 @Data
 @Entity
-public class Feedback {
+public class Feedback extends BaseModel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Lob
     private String comment;
 
     @ManyToOne
@@ -21,4 +26,20 @@ public class Feedback {
 
     @ManyToOne
     private User user;
+
+    public Feedback(String comment, Tool tool, User user) {
+        if (comment == null || comment.isBlank()) {
+            throw new ValidationException("Comment cannot be null or empty.");
+        }
+        if (tool == null) {
+            throw new ValidationException("Tool cannot be null.");
+        }
+        if (user == null) {
+            throw new ValidationException("User cannot be null.");
+        }
+
+        this.comment = comment;
+        this.tool = tool;
+        this.user = user;
+    }
 }
