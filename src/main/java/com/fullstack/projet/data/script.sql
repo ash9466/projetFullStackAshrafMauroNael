80b968fa6c513a3bcdff70ff1e52445449b2a49e
Mauro Simoes <maurosimoes9@gmail.com>
2025-01-10 13:38:17 +0100

    fix db script roles

--- src/main/java/com/fullstack/projet/data/script.sql
+++ src/main/java/com/fullstack/projet/data/script.sql
@@ -8,7 +8,7 @@ CREATE TABLE user (
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role ENUM('ADMIN', 'USER') NOT NULL
+    role ENUM('ADMIN', 'TEACHER', 'STUDENT') NOT NULL
 );
 
 CREATE TABLE tool (
